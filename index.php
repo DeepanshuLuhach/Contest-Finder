b96432acdbb45d0d126654bb2721b552496dcbf2
Deepanshu Luhach <DeepanshuLuhach@users.noreply.github.com>
2017-01-24 00:19:54 +0530

    Minor Fixes

--- index.php
+++ index.php
@@ -7,6 +7,24 @@ $result = curl_exec($ch);
 curl_close($ch);
 $obj = json_decode($result,TRUE);
 
+function mtime($to='Mon, 25 Jan 2017 22:00')
+{
+    $mlist=["Jan","Feb","Mar","April","May","June","July","Aug","Sep","Oct","Nov","Dec"];
+    $seconds=0;
+    list($part1,$part2) = explode(', ', $to);
+    list($day, $mon, $year, $time) = explode(' ', $part2);
+    list($hours, $minutes) = explode(':', $time);
+    $month=0;
+    for($i=0;$i<12;$i++)
+    {
+        if($mlist[$i]==$mon)
+            $month=$i+1;
+
+    }
+    $timeto =  mktime($hours, $minutes, $seconds, $month, $day, $year);
+    return $timeto;
+}
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -19,6 +37,17 @@ $obj = json_decode($result,TRUE);
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </head>
 <body>
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">Contest Reminder</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="#">Up Coming Contests</a></li>
+      <li><a href="#">On Going Contests</a></li>
+    </ul>
+  </div>
+</nav>
 <div class="table-responsive">
   <table class="table">
     <thead>
@@ -34,12 +63,15 @@ $obj = json_decode($result,TRUE);
     <?php
     for($i=0;$i<count($obj['result']['upcoming']);$i++)
     {
-        echo "<tr><td>{$obj['result']['upcoming'][$i]['Name']}</td>
-        <td>{$obj['result']['upcoming'][$i]['StartTime']}</td>
-        <td>{$obj['result']['upcoming'][$i]['Duration']}</td>
-        <td>{$obj['result']['upcoming'][$i]['Platform']}</td>
-        <td>"; ?> <a href="<?php echo $obj['result']['upcoming'][$i]['url']; ?>">Go to Contest</a><?php  echo "</td>
-      </tr>";
+        if(mtime($obj['result']['upcoming'][$i]['StartTime'])>mtime($obj['result']['timestamp']))
+        {   
+            echo "<tr><td>{$obj['result']['upcoming'][$i]['Name']}</td>
+            <td>{$obj['result']['upcoming'][$i]['StartTime']}</td>
+            <td>{$obj['result']['upcoming'][$i]['Duration']}</td>
+            <td>{$obj['result']['upcoming'][$i]['Platform']}</td>
+            <td>"; ?> <a href="<?php echo $obj['result']['upcoming'][$i]['url']; ?>" class="btn btn-info" role="button">Go to Contest</a><?php  echo "</td>
+            </tr>";
+        }
     }
     ?>
     </tbody>
